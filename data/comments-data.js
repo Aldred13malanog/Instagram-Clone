0013cb75d532da97e1d28c672da531c4a8ea860c
Aldred <aldredmalanog@gmail.com>
2024-09-22 21:24:34 +0800

    save the comments data and generate the html

---
+++ data/comments-data.js
@@ -0,0 +1,79 @@
+export const commentsData = [{
+	id: '0',
+	comments: [{
+		username: 'jacoblokietek_',
+		comment: 'bro straightened his neck, stopped pushing out his core, and changed the lighting, wow so impressive'
+	}, {
+		username: 'mainlych3rry',
+		comment: 'Did he went from iphone 13 to Iphone 11?'
+	}, {
+		username: 'lowbeat11',
+		comment: 'No one gonna talk abt the tag'
+	}, {
+		username: 'ted_.bundt.pan_',
+		comment: "I'm so confused"
+	}]
+}, {
+	id: '1',
+	comments: [{
+		username: 'g4ybirey',
+		comment: 'Cok guzel :)'
+	}, {
+		username: 'chulita_pie',
+		comment: "it's like the skies mirror [] it has the reflection all over it []"
+	}, {
+		username: 'mahmud01970',
+		comment: 'Probabliy, moon new moon is borning same hours always, is there regulation'
+	}, {
+		username: 'slyrsth73',
+		comment: "So detailed you can see the were the dust settled from impact craters, and yet no landing sites..."
+	}]
+}, {
+	id: '2',
+	comments: [{
+		username: 'bugatti',
+		comment: 'BUGATTI is the center stage at Chantilly Arts & Elegance Richard Mille - a unique celebration of the art of living, automotive haute couture, and refinement set against the majestic backdrop of Chateau de Chantilly. Here, each detail, much like every BUGATTI crafted, tells a story.'
+	}, {
+		username: 'eddy5968_',
+		comment: "Amazing"
+	}, {
+		username: 'pedalhappytaj',
+		comment: 'Beautiful stories being told'
+	}]
+}, {
+	id: '3',
+	comments: [{
+		username: 'fettyshane13',
+		comment: 'Yeahh ima need slide 2-3 framed!'
+	}, {
+		username: 'thereflexartpatrick',
+		comment: 'Les 24 H du Mans pour 2025 ? 2026 ?'
+	}, {
+		username: 'soul_999956',
+		comment: '1 legend 1 beast'
+	}, {
+		username: 'sneakybruh_',
+		comment: 'My favorite hypercar ever'
+	}]
+}, {
+	id: '4',
+	comments: []
+}, {
+	id: '5',
+	comments: []
+}, {
+	id: '6',
+	comments: []
+}, {
+	id: '7',
+	comments: []
+}, {
+	id: '8',
+	comments: []
+}, {
+	id: '9',
+	comments: []
+}, {
+	id: '10',
+	comments: []
+}];
--- scripts/home-page-scripts.js
+++ scripts/home-page-scripts.js
@@ -1,4 +1,5 @@
 import { fypData } from "../data/fyp-data.js";
+import { commentsData } from "../data/comments-data.js";
 
 // check post fun
 export function checkPost(data) {
@@ -259,74 +260,7 @@ export function viewCommentSection(button, i) {
 				</div>
 			</div>
 			<div class="comments">
-				<div class="comment">
-					<div class="comment-profile-pic"></div>
-					<div class="comment-con">
-						<div class="comment-text">
-							<span class="comment-name">jacoblokietek_</span>
-							bro straightened his neck, stopped pushing out his core, and changed the lighting, wow so impressive
-						</div>
-						<div class="comment-status">
-							<span>3w</span>
-							<span>405 likes</span>
-							<span>Reply</span>
-						</div>
-					</div>								
-					<button class="comment-like-button">
-						<svg aria-label="Like" class="x1lliihq x1n2onr6 x1cp0k07" fill="currentColor" height="12" role="img" viewBox="0 0 24 24" width="12"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
-					</button>
-				</div>
-				<div class="comment">
-					<div class="comment-profile-pic"></div>
-					<div class="comment-con">
-						<div class="comment-text">
-							<span class="comment-name">mainlych3rry</span>
-							Did he went from iphone 13 to Iphone 11?
-						</div>
-						<div class="comment-status">
-							<span>3w</span>
-							<span>167 likes</span>
-							<span>Reply</span>
-						</div>
-					</div>								
-					<button class="comment-like-button">
-						<svg aria-label="Like" class="x1lliihq x1n2onr6 x1cp0k07" fill="currentColor" height="12" role="img" viewBox="0 0 24 24" width="12"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
-					</button>
-				</div>
-				<div class="comment">
-					<div class="comment-profile-pic"></div>
-					<div class="comment-con">
-						<div class="comment-text">
-							<span class="comment-name">lowbeat11</span>
-							No one gonna talk abt the tag
-						</div>
-						<div class="comment-status">
-							<span>3w</span>
-							<span>52 likes</span>
-							<span>Reply</span>
-						</div>
-					</div>								
-					<button class="comment-like-button">
-						<svg aria-label="Like" class="x1lliihq x1n2onr6 x1cp0k07" fill="currentColor" height="12" role="img" viewBox="0 0 24 24" width="12"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
-					</button>
-				</div>
-				<div class="comment">
-					<div class="comment-profile-pic"></div>
-					<div class="comment-con">
-						<div class="comment-text">
-							<span class="comment-name">ted_.bundt.pan_</span>
-							I'm so confused
-						</div>
-						<div class="comment-status">
-							<span>3w</span>
-							<span>52 likes</span>
-							<span>Reply</span>
-						</div>
-					</div>								
-					<button class="comment-like-button">
-						<svg aria-label="Like" class="x1lliihq x1n2onr6 x1cp0k07" fill="currentColor" height="12" role="img" viewBox="0 0 24 24" width="12"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
-					</button>
-				</div>
+				${showComments(info)}				
 			</div>
 			<div class="comment-icon-con">
 				<div class="comment-icon">
@@ -368,9 +302,57 @@ export function viewCommentSection(button, i) {
 		container.removeChild(elem);
 		container.removeChild(overlay);
 		if (info.typePost === 'video') {
-			document.querySelector(`.js-video-${i}`).play();
+			setTimeout(() => {
+				document.querySelector(`.js-video-${i}`).play();
+			}, 500);
 		}
 	});
+
+
+	function showComments(info) {
+		let commentsHtml = '';
+		let matchingData;
+
+		commentsData.forEach(comments => {
+			if (info.id === comments.id) {
+				matchingData = comments;
+			}
+		});
+
+		if (matchingData.comments.length === 0) {
+			commentsHtml = `
+				<div class="no-comment-con">
+					<div class="no-comment-text">No comments yet.</div>
+					<div class="no-comment-text1">Start the conversation.</div>
+				</div>
+			`;
+			return commentsHtml;
+		}
+
+		matchingData.comments.forEach(data => {
+			commentsHtml += `
+				<div class="comment">
+					<div class="comment-profile-pic"></div>
+					<div class="comment-con">
+						<div class="comment-text">
+							<span class="comment-name">${data.username}</span>
+							${data.comment}
+						</div>
+						<div class="comment-status">
+							<span>3w</span>
+							<span>405 likes</span>
+							<span>Reply</span>
+						</div>
+					</div>								
+					<button class="comment-like-button">
+						<svg aria-label="Like" class="x1lliihq x1n2onr6 x1cp0k07" fill="currentColor" height="12" role="img" viewBox="0 0 24 24" width="12"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
+					</button>
+				</div>
+			`;
+		});
+
+		return commentsHtml;
+	}
 
 	if (info.typePost === 'video') {
 		const button = document.querySelector('.csound-button');
@@ -646,7 +628,6 @@ export function viewCommentSection(button, i) {
 		});
 	});
 	
-
 	document.querySelectorAll('.js-cimagevideo-con video').forEach(vid => {
 		vid.addEventListener('click', () => {
 			const parent = vid.parentElement;
@@ -686,5 +667,5 @@ export function viewCommentSection(button, i) {
 				count = 0;
 			}, 500);
 		});
-	})
+	});
 }
